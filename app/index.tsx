a620fdb08b50da2b0146facd909f9c15ae01359d
sdornel <kai_dornel@hotmail.com>
2024-06-06 22:06:22 -0400

    fixed errors

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,23 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-registerRootComponent(App);
+// registerRootComponent(App);
+
+const root = () => {
+    return (
+        <App />
+    )
+}
+
+export default root;
+
+// import { registerRootComponent } from 'expo';
+// import App from './App';
+
+// // Ensure createRoot is only called once
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<App />);
+
+// // Register the app for Expo Go and native environments
+// registerRootComponent(App);
