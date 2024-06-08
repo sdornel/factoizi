33d710043bdb689fb26ced12caae848950ba605d
sdornel <kai_dornel@hotmail.com>
2024-06-07 21:10:53 -0400

    adaugat containerul file

--- app/EcranDeNoteSalvate.tsx
+++ app/EcranDeNoteSalvate.tsx
@@ -1,11 +1,12 @@
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import File from "./containere/File";
 
 const EcranDeNoteSalvate = () => {
     return (
       <View style={styles.container}>
         {/* scroll down and each tab is a "category" */}
         {/* in each category have another screen with a for loop for note components */}
-        
+        <File subiect="programare" />
       </View>
     );
   };
