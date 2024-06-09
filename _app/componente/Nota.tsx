8407a762feffc79ceef0240fc729f704daf54d4d
sdornel <kai_dornel@hotmail.com>
2024-06-08 23:45:04 -0400

    updates

---
+++ _app/componente/Nota.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Nota = () => {
+    return (
+      <View>
+        <Text>nota</Text>
+      </View>
+    );
+  };
+  
+const styles = StyleSheet.create({
+
+});
+  
+export default Nota;
