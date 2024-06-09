3cdcc7978bb1d2480ae6fef78ac4e83cfd55140d
sdornel <kai_dornel@hotmail.com>
2024-06-08 21:43:36 -0400

    cleanup

--- app/containere/File.tsx
+++ app/containere/File.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
-import Nota from "../componente/Nota";
+import { StyleSheet, Text, View } from 'react-native';
+import Nota from '../componente/Nota';
 
 const File = ({ subiect }: { subiect: string}) => {
+  // folosesti async storage aici
     return (
       <View>
         <Text>Subject: {subiect}</Text>
+        <Text></Text>
         <Nota />
       </View>
     );
