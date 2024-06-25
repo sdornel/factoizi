816271a689bf00eed65e7b5caf250b78c42771cd
sdornel <kai_dornel@hotmail.com>
2024-06-24 22:24:56 -0400

    adaugat chesti pe asyncstorage

--- app/containere/Dosar.tsx
+++ app/containere/Dosar.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Nota from '../componente/Nota';
 import { useRoute } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Dosar = () => {
   const route = useRoute();
+  // if no subject i need to redirect to main page
   const { subiect } = route.params as { subiect: string; }; // i should try to do this more nicely in future
+  
+  const [dataArray, setDataArray] = useState<Array<string>>([]);
 
+  useEffect(() => {
+    const retrieveData = async () => {
+      const accessedStorageArray: string | null = await AsyncStorage.getItem(subiect);
+      console.log('accessedStorageArray', accessedStorageArray);
+      if (accessedStorageArray !== null) {
+        setDataArray(JSON.parse(accessedStorageArray));
+      }
+    }
+
+    retrieveData();
+  }, [subiect])
+  console.log('dataArray', dataArray);
   return (
     <View style={styles.container}>
       <Text>Detail Screen for {subiect}</Text>
       {
-        // loop with asyncstorage and nota
+        // AsyncStorage.subiect
       }
       <Nota props={''}/>
     </View>
