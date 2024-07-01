fc6a012b8ccb9ccfaac300fa0f83e7d737d8b02e
sdornel <kai_dornel@hotmail.com>
2024-06-30 22:33:31 -0400

    sters

--- _app/index.tsx
+++
@@ -1,9 +0,0 @@
-import App from './App';
-
-const root = () => {
-    return (
-        <App />
-    )
-}
-
-export default root;
--- _app/types/types.ts
+++
@@ -1,3 +0,0 @@
-export type RootStackParamList = {
-    Fisier: { subiect: string };
-};
